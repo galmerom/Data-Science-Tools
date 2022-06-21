ff891794f5d5126e718ca19b48e2357507d73881
Gal merom <79087871+galmerom@users.noreply.github.com>
2022-06-21 12:45:25 +0300

    fix

--- Util.py
+++ Util.py
@@ -75,6 +75,7 @@ def Scoring(y_true,y_pred,colorSer=None,WithChart=False,Figsize=(10,5),ylabel='P
     r2='{:.3f}'.format(r2_score(y_true, y_pred))
     rmse = '{:.3f}'.format(np.sqrt(mean_squared_error(y_true, y_pred)))
     joinedDF = pd.concat([y_true, y_pred], axis=1)
+    print(joinedDF)
     col1=joinedDF.columns[0]
     col2 = joinedDF.columns[1]
     PercScore = joinedDF.apply(lambda x: __ErorCalc(x[col1], x[col2],NO3_LOD),axis=1).mean()

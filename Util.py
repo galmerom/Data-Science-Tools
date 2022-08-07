eed25b69057faf1409565cf5efcbee7286d78ff8
Gal merom <79087871+galmerom@users.noreply.github.com>
2022-08-07 11:09:55 +0300

    keep changing

--- Util.py
+++ Util.py
@@ -111,7 +111,7 @@ def Scoring(y_true,y_pred,colorSer=None,WithChart=False,Figsize=(10,5),ylabel='P
         else:
             ColorInput = None
             
-        plt.scatter(x=y_true,y=y_pred,c=ColorInput ,label = "label_name")
+        scatter=plt.scatter(x=y_true,y=y_pred,c=ColorInput ,label = "label_name")
         plt.plot([MinValue, MaxValue], [MinValue, MaxValue], 'k-', color = 'r')
 
         # for i, txt in enumerate(rngList):

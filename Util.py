a594934a38fd1612f06a8a1a7bd726849bfd66f5
Gal merom <79087871+galmerom@users.noreply.github.com>
2022-09-28 09:06:27 +0300

    add ConcatDataFrameDict

--- Util.py
+++ Util.py
@@ -227,3 +227,26 @@ def __ErorCalc(y_true,y_pred,LOD):
         return (abs(y_pred-LOD)/LOD)
     else:
         return (abs(y_pred-y_true)/y_pred)
+    
+def ConcatDataFrameDict(DFdic,AddOriginCol=True):
+    """
+    Gets a dictionary of dataframes and perform a concat (on the index axis)
+
+    :param DFdic dictionary. Dictionary with dataframes as the values
+    :param AddOriginCol bool. If true then add 'OriginalKey' column that
+                             contains the key of each dataframe from the dict.
+    Return dataframe. Dataframe that concat all the dictionary dataframes
+    """
+    FirstFlag = True
+    for key in DFdic.keys():
+        if FirstFlag:
+            OutDf = DFdic[key]
+            if AddOriginCol:
+                OutDf['OriginalKey'] = key
+            FirstFlag = False
+        else:
+            tempdf = DFdic[key]
+            if AddOriginCol:
+                tempdf['OriginalKey'] = key
+            OutDf = pd.concat([OutDf,tempdf])
+    return OutDf
